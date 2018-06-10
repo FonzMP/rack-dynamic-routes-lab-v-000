257929203602bfcd9c3a1fc0cc1ee0e33fe908f9
Alex Wilson <fonzmp@gmail.com>
2018-06-10 17:04:05 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -7,18 +7,6 @@ class Application
     resp = Rack::Response.new
     req = Rack::Request.new(env)
 
-    if req.path.match(/items/)
-
-      item_name = req.path.split("/items/").last
-      item = @@items.find{|s| s.name == item_name}
-
-      resp.write item.name
-
-    else
-      resp.write "Route not found"
-      resp.status = 404
-    end
-
   end
 
 end
