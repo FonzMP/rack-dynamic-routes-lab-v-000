b87d22611a622d96b503e62530b7a42d86a18980
Alex Wilson <fonzmp@gmail.com>
2018-06-10 17:13:04 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -12,7 +12,7 @@ class Application
       item = @@items.find {|i| i.name == item_name}
       resp.write item.name
     else
-      return"Page Not Found"
+      resp.write "Page Not Found"
       resp.status = 404
     end
 
