14f6c31ed7a5d19a7c819dbf5e151f5d0dbfa770
Alex Wilson <fonzmp@gmail.com>
2018-06-10 17:13:55 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -10,7 +10,7 @@ class Application
     if req.path.match(/items/)
       item_name = req.path.split("/items/").last
       item = @@items.find {|i| i.name == item_name}
-      resp.write item.name
+      resp.write add_item(item)
     else
       resp.write "Route not found"
       resp.status = 404
